30b75f86e9415a332151363d3be2abe58e35aaa3
lancelot-thomas <121886258+lancelot-thomas@users.noreply.github.com>
2024-10-01 07:18:36 +0000

    exercice prime max score

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+"""Module affichant la liste des nombres premiers"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Vérifie si un nombre est premier.
 
+    Parameters:
+        p (int): Nombre à vérifier
+
+    Returns:
+        bool: Vérifie si p est premier
+    """
     # votre code ici
-
-    pass
+    if p<2 :
+        return False
+    for i in range(2, int(sqrt(p)+1)):
+        if p%i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """
+    Fonction principale affichant parmis les 100 premiers nombres
+    les nombres premiers
+    """
 
     # vos appels à la fonction secondaire ici
 
